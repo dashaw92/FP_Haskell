0aedc78ca8243f976ec44186dd870494d72707a8
dashaw92 <dashaw92@gmail.com>
2022-11-23 17:00:26 -0500

    And unify the naming

--- FP8_Recursive_Functions/merge.hs
+++ FP8_Recursive_Functions/merge.hs
@@ -1,8 +1,8 @@
 -- Taking two sorted lists, merge them together to produce one final sorted list:
 -- merge [2,5,6] [1,3,4] = [1,2,3,4,5,6]
 merge :: Ord a => [a] -> [a] -> [a]
-merge as [] = as
-merge [] bs = bs
+merge xs [] = xs
+merge [] ys = ys
 merge (x:xs) (y:ys) = [min x y, max x y] ++ merge xs ys
 
 -- I don't /think/ this is considered cheating, because I did implement it the expected way?

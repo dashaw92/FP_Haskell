08b8478a29ca2f7604b5c30593680e12d05f860d
dashaw92 <dashaw92@gmail.com>
2022-11-23 12:59:38 -0500

    Improve perfects

--- FP7_List_Comprehensions/perfects.hs
+++ FP7_List_Comprehensions/perfects.hs
@@ -1,7 +1,12 @@
 -- A perfect number is a number where the sum of its factors excluding itself are
 -- equal to the number.
+-- This function finds the first n perfect numbers.
 perfects :: Int -> [Int]
-perfects n = [x | x <- [1..n], sumFactors x == x]
+perfects n = take n allPerfects
+
+-- Computes an infinite sequence of perfect numbers
+allPerfects :: [Int]
+allPerfects = [x | x <- [1..], sumFactors x == x]
     where
         sumFactors as = sum (factors as)
 
